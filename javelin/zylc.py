fix the time normalization bug in the covariance calculation under PL model, and implemented the getcmat_lcross_PL() function.

javelin/zylc.py
@@ -70,7 +70,7 @@ class LightCurve(object):
         # jlist/mlist/elist/ilist: list of j, m, e, i of each individual light curve 
         self.jlist, self.mlist, self.elist, self.ilist = self.sorteddatalist(self.zylclist)
         # rlist: list of ratios of each line+cont band to the pure cont band light curve
-        self.rlist = [np.array(self.mlist[i]) / np.array(self.mlist[0]) for i in range(len(self.mlist))]
+        self.rlist = [np.mean(self.mlist[i]) / np.mean(self.mlist[0]) for i in range(len(self.mlist))]
 
         # continuum properties, useful in determining continuum variability
         self.cont_mean     = np.mean(self.mlist[0])
@@ -579,6 +579,11 @@ def get_data(lcfile, names=None, set_subtractmean=True, timeoffset=0.0, dat_type
             # rescale the light curves.
             lc[0][1] = [lc[0][1][i] * rescale for i in range(len(lc[0][1]))]
             lc[0][2] = [lc[0][2][i] * rescale for i in range(len(lc[0][2]))]
+
+            mask = np.isinf(np.array(lc[0][1]))
+            lc[0][0] = np.ma.array(np.array(lc[0][0]), mask=mask).compressed()
+            lc[0][1] = np.ma.array(np.array(lc[0][1]), mask=mask).compressed()
+            lc[0][2] = np.ma.array(np.array(lc[0][2]), mask=mask).compressed()
 
 
             lclist.append(lc[0])